888c7a39cc37af04121164a8900c2ef443fb589d
Kevin McManus <kevin.leo.mcmanus@gmail.com>
2023-04-20 06:52:50 -0600

    demo script for animation

---
+++ RunAnimation.m
@@ -0,0 +1,8 @@
+time_t = [0:0.001:10];
+IP = InvertedPendulum();
+%V0 = [0;0; -0.175;0]; % initial bar tilt 10 degrees
+V0 = [0;0; -0.175;0];
+Force = 2.5;
+figure(Name='With lqr')
+sr = IP.euler_method(V0, Force, time=time_t, lqr=true);
+sr.animate()
